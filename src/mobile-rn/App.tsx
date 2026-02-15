2cf7141c53d105896d2650d3b5304fc3f59dd6a0
zcy <12455251+zcy2233@user.noreply.gitee.com>
2026-02-15 20:18:07 +0800

    feat：添加Reactotron调试工具

--- src/mobile-rn/App.tsx
+++ src/mobile-rn/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
 
+if (__DEV__) {
+  require('./src/ReactotronConfig');
+}
+
 export default function App() {
   return (
     <>
